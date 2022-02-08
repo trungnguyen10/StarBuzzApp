e7b7a0195fe301847555f9d7bf16c7204a897a0b
Trung Nguyen <trungnguyen2809@gmail.com>
2022-02-08 10:40:39 -0600

    Create dotnet project and library

---
+++ StarBuzzApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using StarBuzzLib;
+
+namespace StarBuzzApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
